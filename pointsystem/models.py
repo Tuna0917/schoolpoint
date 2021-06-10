e673e7dd03ba4b2a510f9cd2f1780caa799bbb94
Tuna0917 <67810535+Tuna0917@users.noreply.github.com>
2021-06-10 23:18:55 +0900

    이제 만들면 되나?

--- pointsystem/models.py
+++ pointsystem/models.py
@@ -11,10 +11,23 @@ class Teacher(models.Model):
 
 
 class Student(models.Model):
+    STATUS = (
+        ('a', '재학'), #Attending
+        ('b', '휴학'), #Break
+        ('d', '자퇴'), #Dropout
+        ('k', '퇴학') #Kicked
+    )
     name = models.CharField(max_length=32) #이름
-    num = models.IntegerField() #번호
-    point = models.IntegerField(default=0)
+    num = models.IntegerField(validators=[MinValueValidator(1),]) #번호
+    point = models.IntegerField(default=0,validators=[MinValueValidator(0),])
     room = models.ForeignKey('Room', on_delete=models.SET_NULL, null=True) #반
+    status = models.CharField(
+        max_length=1,
+        choices=STATUS,
+        blank=True,
+        default='a'
+    )
+
 
     def __str__(self):
         return self.name
@@ -22,12 +35,19 @@ class Student(models.Model):
 
 class Seat(models.Model):
     student = models.OneToOneField(Student, on_delete=models.SET_NULL, null=True)
-    room = models.ForeignKey('Room', on_delete=models.SET_NULL, null=True)
+    room = models.ForeignKey('Room', on_delete=models.CASCADE)
 
 
 class Room(models.Model):
     grade = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(12)]) #학년
     num = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(63)]) #반
+    line = models.IntegerField(validators=[MinValueValidator(1),])
 
     def __str__(self):
         return f"{self.grade}학년 {self.num}반"
+
+class SeatLog(models.Model):
+    date = models.DateTimeField(auto_now_add=True) #언제
+    student = models.ForeignKey(Student, on_delete=models.DO_NOTHING) #누가
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE) #어떤 좌석을
+    points = models.IntegerField(validators=[MinValueValidator(0),]) #얼마에
--- pointsystem/views.py
+++ pointsystem/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#Create Room
+'''
+어떻게 만들어야 잘 만들었다고 소문이 날까
+줄 갯수 입력하고 좌석 갯수 정하면 될려나
+'''
